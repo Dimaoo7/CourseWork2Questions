c16b94c2f94955fbe00cfaf43644d480a4d8489a
Дмитрий <dimakrjukov@gmail.com>
2023-07-07 15:34:00 +0200

    Model was created. Project was started

---
+++ src/main/java/pro/sky/coursework2questions/CourseWork2QuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2QuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2QuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/coursework2questions/CourseWork2QuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2QuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
