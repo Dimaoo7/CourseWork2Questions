193a3efbb270aeb779a7bb9b21ae53bc9ef03324
Дмитрий <dimakrjukov@gmail.com>
2023-07-09 20:52:20 +0200

    Little rework 2.0

--- src/main/java/pro/sky/coursework2questions/service/ExaminerServiceImpl.java
+++ src/main/java/pro/sky/coursework2questions/service/ExaminerServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 public class ExaminerServiceImpl implements ExaminerService {
 
-    private final List<String> questionsList = new ArrayList<>();
+
     private final QuestionService questionService;
 
     public ExaminerServiceImpl(QuestionService questionService) {
@@ -22,30 +22,16 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     @Override
     public List<String> getQuestions(int amount) {
-        questionsList.clear();
+        List<String> questionsList = new ArrayList<>();
         validateQuantityQuestions(amount);
-        int originalQuestionsCounter = 1;
-        while (originalQuestionsCounter <= amount) {
-            String question = questionService.getRandomQuestion().getQuestion();
-            if (isQuestionUnique(question)) {
-                questionsList.add(question);
-                originalQuestionsCounter++;
-            }
+        if (questionsList.toString().contains(questionService.getRandomQuestion().getQuestion())) {
+            questionsList.add(questionService.getRandomQuestion().getQuestion());
         }
         return questionsList;
     }
 
-    private boolean isQuestionUnique(String question) {
-        for (String element : questionsList) {
-            if (element.equals(question)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     private void validateQuantityQuestions(int amount) {
-        if (amount < questionService.getAll().size() || amount > 0) {
+        if (amount > questionService.getAll().size() || amount < 0) {
             throw new AmountOutOfCollectionBoundException();
         }
     }
